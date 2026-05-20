94b96521acc70e0249d33c7deb01c8bfbb9781bc
bariskarapinar <bkarapinar1@hotmail.com>
2026-05-20 17:49:22 +0300

    MainActivity.kt Updated

--- app/src/main/java/com/myapp/mywallet/MainActivity.kt
+++ app/src/main/java/com/myapp/mywallet/MainActivity.kt
@@ -1,16 +1,15 @@
 package com.myapp.mywallet
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.fragment.app.FragmentActivity
 import com.myapp.mywallet.ui.screens.WalletHomeScreen
 import com.myapp.mywallet.ui.theme.MyWalletTheme
-import dagger.hilt.android.EntryPointAccessors
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
